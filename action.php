fef7d881b5958b187e863cd3da548122956a558d
Gregory Loichot <gregory.loichot@unige.ch>
2024-05-28 16:07:09 +0200

    Remove deprecated instruction

--- action.php
+++ action.php
@@ -13,8 +13,6 @@ if(!defined('DOKU_LF')) define('DOKU_LF', "\n");
 if(!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
 
-require_once DOKU_PLUGIN.'action.php';
-
 class action_plugin_singlesearchresult extends DokuWiki_Action_Plugin {
 
     public function register(Doku_Event_Handler $controller) {
